6cb0c28fd7021def053cfd2d6cc2b56bb3c9ef56
Guillermo <guillermotunes@hotmail.com>
2023-06-02 06:11:13 -0300

    create models en home

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    def __str__(self):
+        return self.titulo
+
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
