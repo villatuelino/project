283e6a6ac996f9c4af01bb1ce73c5f8225ab8d0c
Guillermo <guillermotunes@hotmail.com>
2023-06-02 05:15:49 -0300

    Create requirements

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import sys
+
+APLICACIONES = BASE_DIR / "apps"
+sys.path.append(str(APLICACIONES))
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -37,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "home",
 ]
 
 MIDDLEWARE = [
